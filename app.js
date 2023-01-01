d677c580ede93e2e99bd22be46a236a3d7cecc30
渡邉将太 <shotawebcoding@gmail.com>
2023-01-02 00:25:50 +0900

    push

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log('ここにコードを追加します!');
+const e1 = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date,
+};
+// 高度な型　型ガード
+function add(a, b) {
+    if (typeof a === 'string' || b === 'string') {
+        return a.toString() + b.toString();
+    }
+    else {
+        return Number(a) + Number(b);
+    }
+}
+function printEmployeeInfomation(emp) {
+    console.log(emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("startDate: " + emp.startDate);
+    }
+}
+printEmployeeInfomation(e1);
--- app.ts
+++ app.ts
@@ -1,1 +1,48 @@
-console.log('ここにコードを追加します!');
+// 高度な型　交差型
+type Admin = {
+    name: string;
+    privileges: string[];
+}
+
+type Employee = {
+    name: string;
+    startDate: Date;
+}
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date,
+}
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+
+// 高度な型　型ガード
+function add(a: Combinable, b: Combinable) { 
+    if (typeof a === 'string' || b === 'string') {
+        return a.toString() + b.toString();
+    } else { 
+        return Number(a) + Number(b);
+    }
+    
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfomation(emp: UnknownEmployee) { 
+    console.log(emp.name);
+    if ("privileges" in emp) { 
+        console.log("Privileges: " + emp.privileges)
+    }
+    if ("startDate" in emp) { 
+        console.log("startDate: " + emp.startDate)
+    }
+}
+
+printEmployeeInfomation({name:'Manu', startDate: new Date()});
