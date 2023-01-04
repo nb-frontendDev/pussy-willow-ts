537562147abdf1da1c108fd08097bf39a055d63e
渡邉将太 <shotawebcoding@gmail.com>
2023-01-04 23:57:38 +0900

    push

--- generics.js
+++ generics.js
@@ -1,2 +1,15 @@
 "use strict";
-const names = [];
+// const names: Array<string> = [];
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => { 
+//         resolve('')
+//     }, 2000)
+// });
+// promise.then((data) => { 
+//     data.split(' ');
+// })
+function merge(objA, ojbB) {
+    return Object.assign(objA, ojbB);
+}
+const mergedObj = merge({ name: 'Max' }, { age: 30 });
+console.log(mergedObj.age);
--- generics.ts
+++ generics.ts
@@ -1,1 +1,18 @@
-const names = [];
+// const names: Array<string> = [];
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => { 
+//         resolve('')
+//     }, 2000)
+// });
+
+// promise.then((data) => { 
+//     data.split(' ');
+// })
+
+function merge<T, U>(objA: T, ojbB: U) { 
+    return Object.assign(objA, ojbB);
+}
+
+const mergedObj = merge({ name: 'Max' }, { age: 30 });
+console.log(mergedObj.age);
