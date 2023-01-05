0b86553fd462c608bf1236d34c13887df0a1671d
渡邉将太 <shotawebcoding@gmail.com>
2023-01-05 23:22:11 +0900

    push

--- generics.ts
+++ generics.ts
@@ -14,5 +14,5 @@ function merge<T, U>(objA: T, ojbB: U) {
     return Object.assign(objA, ojbB);
 }
 
-const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObj.age);
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] },30);
+console.log(mergedObj);
