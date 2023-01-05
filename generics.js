1317bdbd84155093c11b8acfa520c971c5fa8bf2
渡邉将太 <shotawebcoding@gmail.com>
2023-01-05 23:41:28 +0900

    push

--- generics.js
+++ generics.js
@@ -13,3 +13,11 @@ function merge(objA, ojbB) {
 }
 const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 console.log(mergedObj);
+function countAndDescribe(element) {
+    let descriptionText = '値がありません。';
+    if (element.length > 0) {
+        descriptionText = '値は' + element.length + '個です。';
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe([]));
